2500c65502e8c4c54d259e173076c2d5ed4dda7b
AldJayR <aldwinjzs3@gmail.com>
2024-10-04 00:03:22 +0800

    Renamed image names

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Gallery />
     </>
   )
 }
